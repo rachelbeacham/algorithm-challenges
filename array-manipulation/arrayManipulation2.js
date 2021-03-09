5be36aa9ca79bd1bfd464e3f55ef6290d67b4f5b
Rachel Beacham <rachelbeacham17@gmail.com>
2021-03-09 01:21:35 +0000

    Added drop function

--- array-manipulation/arrayManipulation2.js
+++ array-manipulation/arrayManipulation2.js
@@ -14,3 +14,20 @@ function take(array, count) {
   const newArray = array.slice(0, count)
   return newArray;
 }
+
+
+/*🚨🚨🚨 drop(array, count)
+
+Parameters
+array - Any JavaScript Array
+count - A positive integer Number of elements to "drop" from the beginning of array
+
+Return Value
+A new Array containing the elements of array after the first count elements.
+
+Do not mutate the input array! */
+
+function drop(array, count) {
+  const newArray = array.slice(count);
+  return newArray;
+}
