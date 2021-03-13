4808c6915e3beb36e8f0941fb255830b3a7f0673
Rachel Beacham <rachelbeacham17@gmail.com>
2021-03-13 01:51:15 +0000

    Deleted unnecessary files.

--- array-manipulation/takeRight.js
+++
@@ -1,7 +0,0 @@
-/* Define a function that returns a new Array containing the last however many
-elements of a given array */
-
-function takeRight(array, count) {
-  const newArray = array.slice(-count);
-  return newArray;
-}
