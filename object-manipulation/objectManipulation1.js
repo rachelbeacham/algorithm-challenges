bd68b19e38f65f6a239ad8ec8ed83651f232a030
Rachel Beacham <rachelbeacham17@gmail.com>
2021-03-16 00:07:56 +0000

    Added setValue function

--- object-manipulation/objectManipulation1.js
+++ object-manipulation/objectManipulation1.js
@@ -14,3 +14,18 @@ function getValue(object, key) {
 
 // ES6
 const value = (object, key) => object[key];
+
+
+/* 🚨🚨🚨  setValue(object, key, value)
+
+Parameters
+object - Any JavaScript Object
+key - A JavaScript String
+value - Any JavaScript value
+
+Return Value
+This function does not return anything (undefined) */
+
+function setValue(object, key, value) {
+  object[key] = value;
+}
